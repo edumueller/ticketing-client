4ef76a7d576d6ff353c08c3fa2d66880287cf0f5
edumueller <eduardo.nicastro@icloud.com>
2021-08-20 12:52:44 -0300

    feat: nginx config for server-side render calls

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // we are on the server
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: {
+            Host: "ticketing.dev",
+          },
+        }
+      )
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    return data;
+  } else {
+    // we are on the browser
+    const { data } = await axios.get("/api/users/currentuser").catch((err) => {
+      console.log(err.message);
+    });
+
+    return data;
+  }
+};
+
+export default LandingPage;
