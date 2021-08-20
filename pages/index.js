376b42d49d3f8585faeb97bec89a115fdc08bb64
edumueller <eduardo.nicastro@icloud.com>
2021-08-20 16:42:27 -0300

    feat: conditionally render msg when logged in

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
 
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>Welcome, {currentUser.email}</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  const client = buildClient({ req });
+// context = { req, res }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser");
   return data;
 };
