d3ae36d5395e5bf7a48f18ab37e1b85de362e87c
edumueller <eduardo.nicastro@icloud.com>
2021-09-03 20:35:36 -0300

    implements stripe component and payment

--- pages/orders/[orderId].js
+++ pages/orders/[orderId].js
@@ -1,7 +1,17 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -20,7 +30,16 @@ const OrderShow = ({ order }) => {
   return timeLeft < 0 ? (
     <div>Order Expired</div>
   ) : (
-    <div>Time left to pay: {timeLeft} seconds</div>
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51JVf7HGOpTW8ZUzuonJ0DxlFxd3WjalQaLwCl0jmEcVaEfwJ31vYvx4DoBDNuWEUcqa0ZIcKqadtqaD02QaYXgFs00ngLK1vtA"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
   );
 };
 
